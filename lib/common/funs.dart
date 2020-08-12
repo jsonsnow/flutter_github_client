0816638d74bb52342701c259e589fa74698c5245
jsonsnow <1183010734@qq.com>
2020-08-12 20:20:43 +0800

    feat<login>

---
+++ lib/common/funs.dart
@@ -0,0 +1,78 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+Widget gmAvatar(String url,
+    {double width = 30, double height, BoxFit fit, BorderRadius borderRadius}) {
+  var plcaeholder = Image.asset(
+    'imgs/avatar-default.png',
+    width: width,
+    height: height,
+  );
+  return ClipRRect(
+    borderRadius: borderRadius,
+    child: CachedNetworkImage(
+      imageUrl: url,
+      width: width,
+      height: height,
+      fit: fit,
+      placeholder: (context, url) => plcaeholder,
+      errorWidget: (context, url, error) => plcaeholder,
+    ),
+  );
+}
+
+void showToast(
+  String text, {
+  gravity: ToastGravity.CENTER,
+  toastLength: Toast.LENGTH_SHORT,
+}) {
+  Fluttertoast.showToast(
+      msg: text,
+      toastLength: toastLength,
+      gravity: gravity,
+      timeInSecForIosWeb: 1,
+      backgroundColor: Colors.grey[600],
+      fontSize: 16.0);
+}
+
+void showLoading(BuildContext context, [String text]) {
+  String msg = text ?? 'loading...';
+  showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (context) {
+        return Center(
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(3.0),
+              boxShadow: [BoxShadow(color: Colors.black12, blurRadius: 10)],
+            ),
+            padding: EdgeInsets.all(16),
+            margin: EdgeInsets.all(16),
+            constraints: BoxConstraints(minHeight: 120, minWidth: 180),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  height: 30,
+                  width: 30,
+                  child: CircularProgressIndicator(
+                    strokeWidth: 3,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 20.0),
+                  child: Text(
+                    msg,
+                    style: Theme.of(context).textTheme.bodyText2,
+                  ),
+                )
+              ],
+            ),
+          ),
+        );
+      });
+}
